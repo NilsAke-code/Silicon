9f569f9e52bba1a48bf486ee570a666801e53e74
NilsAke-code <nils.lindqvist36@gmail.com>
2024-11-23 11:56:25 +0100

    Error fixed

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Hej och välkommen");
-Console.WriteLine("Var vänligen och fyll i informationen här nedan.";
+Console.WriteLine("Var vänligen och fyll i informationen här nedan.");
 
 Console.Write("Förnamn:");
 string firstName = Console.ReadLine()!;
@@ -7,5 +7,5 @@ string firstName = Console.ReadLine()!;
 Console.Write("Efternamn:");
 string lastName = Console.ReadLine()!;
 
-
+ 
 
